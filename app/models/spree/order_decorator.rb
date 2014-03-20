af9a4eb6dbcb453d5ad4a7a68e78a4ce758ff4df
Hugo Santos <hugo.santos@factorlibre.com>
2014-03-20 18:14:16 +0100

    HotFix: Not use hooks on update order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,9 +1,9 @@
 Spree::Order.class_eval do
   has_one :source, class_name: 'Spree::Chimpy::OrderSource'
 
-  register_update_hook :notify_mail_chimp
+  #register_update_hook :notify_mail_chimp
 
-  around_save :handle_cancelation
+  #around_save :handle_cancelation
 
   def notify_mail_chimp
     Spree::Chimpy.enqueue(:order, self) if completed? && Spree::Chimpy.configured?
